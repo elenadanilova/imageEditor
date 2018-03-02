6fff0537318944aebdc41c4e1dae57b735df5723
e.danilova <e.danilova@playa.ru>
2018-03-02 16:07:56 +0300

    refactor project

--- src/script.js
+++ src/script.js
@@ -1,47 +1,47 @@
-$(document).ready(function() {
-    
-    function fileSelect(evt) {
-		evt.stopPropagation();
-		evt.preventDefault();
-		if (window.File && window.FileReader && window.FileList && window.Blob) {
-			var files = evt.dataTransfer.files;
-	 
-			var result = '';
-			var file;
+/**
+ * created by e.danilova@playa.ru 01.03.2018
+ */
+'use strict'
 
+function imageEditor(options) {
+	this.ratio = options.ratio;
+	var dropZone = document.getElementById(options.dropZone);
+	
+	this.addDragEvents = function() {	
+		dropZone.addEventListener('dragover',function(){
+			event.preventDefault();
+			event.stopPropagation();
+			event.dataTransfer.dropEffect = 'copy';			
+		},false);
+		
+		dropZone.addEventListener('drop',function(){
+			event.preventDefault();
+			event.stopPropagation();
+			selectFile();
+		},false);		
+	}
+	
+	var selectFile = function() {		
+		if(window.File && window.FileReader && window.FileList && window.Blob) {
+			//console.log('drag and drop is worked');
+			var files = event.dataTransfer.files;
+			var file;
+			
 			for (var i = 0; file = files[i]; i++) {
-				
-				// if the file is not an image, continue
+				//console.log(i);
+				//console.log(file);
 				if (!file.type.match('image.*')) {
 					continue;
 				}
-	 
-				reader = new FileReader();
-				reader.onload = (function (tFile) {
-					return function (evt) {
-						var div = document.createElement('div');
-						dropTarget.innerHTML = '<img style="max-width: 100%; position: relative"; src="' + evt.target.result + '" />';
-						document.getElementById('filesInfo').appendChild(div);
-					};
-				}(file));
-				reader.readAsDataURL(file);
 				
-				result += '<li>' + file.name + ' ' + file.size + ' bytes</li>';
+				var reader = new FileReader();
+				reader.onload = function(file) {										
+					dropZone.innerHTML = '<img style="max-width: 100%; position: relative"; src="' + event.target.result + '" />';					
+					console.log(dropZone);
+				}
+				reader.readAsDataURL(file);
 			}
-			document.getElementById('filesInfo').innerHTML = '<ul>' + result + '</ul>';
-		} else {
-			alert('The File APIs are not fully supported in this browser.');
+			
 		}
 	}
- 
-	function dragOver(evt) {
-		evt.stopPropagation();
-		evt.preventDefault();
-		evt.dataTransfer.dropEffect = 'copy';
-	}
-	 
-	var dropTarget = document.getElementById('dropTarget');
-	dropTarget.addEventListener('dragover', dragOver, false);
-	dropTarget.addEventListener('drop', fileSelect, false);
-    
-});
+};
